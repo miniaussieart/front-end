7004115a3bab76a8b071e4cec1c8e838fb6fa1e8
Austin Hedeen <austin.hedeen@gmail.com>
2019-11-05 22:13:37 -0800

    Convert UI to Vuetify

--- src/components/PriceTierList.vue
+++ src/components/PriceTierList.vue
@@ -1,55 +1,144 @@
 <template>
-<div class="price-tier-list">
-  <ul>
-    <li v-for="tier in tierList" v-bind:key="tier.id">
-      <p>{{ tier.category }}</p>
-      <p>{{ tier.minimumPrice }}</p>
-      <p>{{ tier.description }}</p>
-      <ul class="tier">
-        <li v-for="example in tier.examples" v-bind:key="example.id">
-          <img :src="example.src"/>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</div>
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col
+        v-for="tier in tierList"
+        :key="tier.id"
+        cols="12"
+        sm="6"
+      >
+        <h1>{{ tier.category }}</h1>
+        <p>{{ tier.minimumPrice }}</p>
+        <p>{{ tier.description }}</p>
+        <v-carousel>
+          <v-carousel-item
+            v-for="example in tier.examples"
+            :key="example.id"
+            :src="example.src"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-
   name: 'PriceTierList',
-  data() {
-    return {
-      count: 0,
-      alert: 'Prices will increase November 2019',
-      tierList: [
-        {
-          id: Math.random(),
-          category: 'Chibi Headshots',
-          minimumPrice: 'Starting at $8',
-          description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
-          examples: [
-            {
-              src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
-            },
-            {
-              src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
-            },
-            {
-              src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
-            },
-          ],
-        },
-      ],
-    };
-  },
+  data: () => ({
+    count: 0,
+    alert: 'Prices will increase November 2019',
+    tierList: [
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        category: 'Chibi Headshots',
+        minimumPrice: 'Starting at $8',
+        description: 'Choose your style and render! Additional fee for multiple characters and complex designs.',
+        examples: [
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435193/otters/d4ixxx1whrcxjrywizmw.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570435175/otters/fwbtiixp54frxzmgteea.jpg',
+          },
+          {
+            src: 'https://res.cloudinary.com/dkjk6wkyc/image/upload/v1570434515/otters/nyyrddarkk41wa6gtimr.jpg',
+          },
+        ],
+      },
+    ],
+  }),
 };
 </script>
 
 <style scoped>
-img {
-  height: 100px;
-  max-width: 200px;
-}
+  img {
+    height: 100px;
+    max-width: 200px;
+  }
 </style>
--- src/views/Prices.vue
+++ src/views/Prices.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="prices">
-    <h1>MINIAUSSIEART</h1>
+    <h1 class="custom-style-1 logo text-uppercase">
+      <span>MiniAussie</span>
+        <span class="logo-red">A</span>
+        <span class="logo-blue">r</span>
+        <span class="logo-green">t</span>
+    </h1>
 
-    <h2>●COMMISSION PRICES●</h2>
-    <p>{{ alert }}</p>
-    <p>All listed prices are subject to increase depending on Character Complexity.</p>
-    <p>Simple backgrounds are included in all commission types.</p>
-    <p>WEBSITE IS CURRENTLY UNDER CONSTRUCTION</p>
+    <h2 class="custom-style-2">●COMMISSION PRICES●</h2>
+    <p class="custom-style-3">{{ alert }}</p>
+    <p class="custom-style-4">
+      All listed prices are subject to increase depending on Character Complexity.
+    </p>
+    <p class="custom-style-5">
+      Simple backgrounds are <span class="highlight-green">included</span> in all commission types.
+    </p>
+    <p class="custom-style-6">WEBSITE IS CURRENTLY UNDER CONSTRUCTION</p>
 
-    <hr>
+    <v-divider></v-divider>
 
     <PriceTierList></PriceTierList>
   </div>
@@ -22,5 +31,69 @@ export default {
   components: {
     PriceTierList,
   },
+  data: () => ({
+    alert: 'Prices will increase November 2019',
+  }),
 };
 </script>
+
+<style scoped>
+  .prices {
+    text-align: center;
+  }
+
+  .logo {
+    color: #50c0cd;
+  }
+
+  .logo-red {
+    color: #ef5454;
+  }
+
+  .logo-blue {
+    color: #4fb7e7;
+  }
+
+  .logo-green {
+    color: #9bd33a;
+  }
+
+  .highlight-green {
+    color: rgb(78, 154, 8);
+  }
+
+  .custom-style-1 {}
+
+  .custom-style-2 {
+    color: #333333;
+    font-size: 32px;
+    font-weight: 400;
+  }
+
+  .custom-style-3 {
+    color: #000000;
+    font-size: 13px;
+    font-weight: 400;
+    text-decoration: underline;
+  }
+
+  .custom-style-4 {
+    color: #000000;
+    font-size: 16px;
+    font-weight: 400;
+  }
+
+  .custom-style-5 {
+    color: #2a2a2a;
+    font-size: 16px;
+    font-weight: 400;
+    text-decoration: underline;
+  }
+
+  .custom-style-6 {
+    color: #d80000;
+    font-size: 24px;
+    font-weight: 700;
+    text-decoration: underline;
+  }
+</style>
